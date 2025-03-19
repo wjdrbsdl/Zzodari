043b80ab7a30860a63d753d79ec880e1c1de7ad8
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-19 11:48:11 +0900

    [추가] 손패 카드 이동 1. 카드 드래그가 끝났다면 해당 카드를 손, 선택존 이동 진행

--- Assets/Scripts/Play/CardManager.cs
+++ Assets/Scripts/Play/CardManager.cs
@@ -7,10 +7,15 @@ public class CardManager : MonoBehaviour
 {
     public static CardManager intance;
 
-    public ArrangeCardObj m_arrangeCard;
+    public ArrangeCardObj m_arrangeHandCard;
+    public ArrangeCardObj m_arrangeSelectCard;
     public Transform cardHands;
     public CardObject cardSample;
     public CardObject[] cards;
+
+    public List<CardData> m_haveCardList = new List<CardData>();
+    public List<CardData> m_handCardList = new List<CardData>();
+    public List<CardData> m_selectList = new List<CardData>();
 
     void Awake()
     {
@@ -48,6 +53,7 @@ public class CardManager : MonoBehaviour
 
     public void SetHaveCard(List<CardData> _haveCardList)
     {
+        m_haveCardList = _haveCardList;
         //있는 만큼 켜고 세팅
         for (int i = 0; i < _haveCardList.Count; i++)
         {
@@ -61,6 +67,41 @@ public class CardManager : MonoBehaviour
         }
 
         CardObject[] activeCards = cardHands.GetComponentsInChildren<CardObject>();
-        m_arrangeCard.ArrangePosition(activeCards);
+        m_arrangeHandCard.SetCardObjects(activeCards);
+    }
+
+    public void EndDrag(CardObject _object, bool _isSelectZone)
+    {
+        Debug.Log("카드를 선택존에 놓았는가" + _isSelectZone);
+        //카드 드래그를 끝냈을때, 끝낸 위치가 셀렉존인가. 
+        if(_object.m_isCurSelect == _isSelectZone)
+        {
+            //있던 위치에 놓은 애는 별 조작 없음
+            return;
+        }
+
+        if (_isSelectZone)
+        {
+            DragHandToSelect(_object);
+        }
+        else
+        {
+            DragSelectToHand(_object);
+        }
+    }
+
+    private void DragHandToSelect(CardObject _object)
+    {
+        // 드래그중이던 카드를 셀렉존에 놓았을때
+        m_arrangeHandCard.RemoveCardObject(_object);
+        m_arrangeSelectCard.AddCardObject(_object);
+        _object.m_isCurSelect = true;
+    }
+
+    private void DragSelectToHand(CardObject _object)
+    {
+        m_arrangeSelectCard.RemoveCardObject(_object);
+        m_arrangeHandCard.AddCardObject(_object);
+        _object.m_isCurSelect = false;
     }
 }
