980449abc48eff822407ec8388e3094a0697faad
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-19 11:46:00 +0900

    [수정] 카드 정렬 구조 1. 정렬할 카드를 세팅하는 함수 2. 이후 카드를 넣거나 빼는 함수

--- Assets/Scripts/Play/ArrangeCardObj.cs
+++ Assets/Scripts/Play/ArrangeCardObj.cs
@@ -7,28 +7,47 @@ public class ArrangeCardObj : MonoBehaviour
 {
     public Transform m_centerTrans; //기준점 
     public float m_xGap = 2f;
-    public void ArrangePosition(CardObject[] _cardObjectList)
+    public List<CardObject> arrangeList = new();
+
+    public void SetCardObjects(CardObject[] _cardObjects)
+    {
+        for (int i = 0; i < _cardObjects.Length; i++)
+        {
+            arrangeList.Add(_cardObjects[i]);
+        }
+        ArrangePosition();
+    }
+
+    private void ArrangePosition()
     {
         //가운데 정렬 
         Vector3 arrangePos = m_centerTrans.position;
-        int half = _cardObjectList.Length / 2;
-        int rest = _cardObjectList.Length % 2;
+        int half = arrangeList.Count / 2;
+        int rest = arrangeList.Count % 2;
         float startX = half * m_xGap * -1;
         if(rest == 0)
         {
             startX += m_xGap / 2;
         }
         arrangePos.x += startX;
-        for (int i = 0; i < _cardObjectList.Length; i++)
+        for (int i = 0; i < arrangeList.Count; i++)
         {
-            _cardObjectList[i].SetArrangePos(arrangePos);
+            arrangeList[i].SetArrangePos(arrangePos);
             arrangePos.x += m_xGap;
         }
 
     }
 
-    void Update()
+    public void AddCardObject(CardObject _object)
     {
-        
+        arrangeList.Add(_object);
+        ArrangePosition();
     }
+
+    public void RemoveCardObject(CardObject _object)
+    {
+        arrangeList.Remove(_object);
+        ArrangePosition();
+    }
+
 }
