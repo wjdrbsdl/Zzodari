65ae8b8060ad7a77b17e6ab7a5735ad2fa61d5d8
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-26 11:14:14 +0900

    [수정] 엔디안 변환 함수 추가 1. 변환 부분 클래스로 분리 2. 클라, 서버 send, recv에서 적용

---
+++ Assets/Scripts/EndianChanger.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Net;
+
+public class EndianChanger
+{
+
+    public static ushort NetToHost(byte[] _bigEndians)
+    {
+        return (ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_bigEndians));
+    }
+
+    public static byte[] HostToNet(ushort _length)
+    {
+        byte[] lengthByte = BitConverter.GetBytes((ushort)IPAddress.HostToNetworkOrder((short)_length));
+        return lengthByte;
+    }
+}
+
