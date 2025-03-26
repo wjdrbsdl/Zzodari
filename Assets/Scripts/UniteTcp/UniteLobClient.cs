b46fbeff779a0b9b9cade875065e4fcd8fa191bb
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-26 17:06:51 +0900

    [추가] 방 리스트 가져오기

--- Assets/Scripts/UniteTcp/UniteLobClient.cs
+++ Assets/Scripts/UniteTcp/UniteLobClient.cs
@@ -7,15 +7,22 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UniteLobClient : MonoBehaviour
 {
     public Socket clientSocket;
+    public InputField inputText;
     public int port = 5000;
     public string ip;
     public int id;
     public MeetState meetState = MeetState.Lobby;
     public string RoomName = "";
+
+    public UniteLobClient()
+    {
+
+    }
 
     public UniteLobClient(string _ip, int _id, string _preRoomName = "")
     {
@@ -34,7 +41,6 @@ public class UniteLobClient : MonoBehaviour
         IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
         byte[] buff = new byte[100];
         clientSocket.BeginConnect(endPoint, CallBackConnect, buff);
-        //Update();
     }
 
     public void ReConnect()
@@ -43,7 +49,6 @@ public class UniteLobClient : MonoBehaviour
         IPEndPoint endPoint = new IPEndPoint(UniteServer.ServerIp, port);
         byte[] buff = new byte[100];
         clientSocket.BeginConnect(endPoint, CallBackConnect, buff);
-        //Update();
     }
 
     public void OnClickReqRoomJoin()
@@ -60,6 +65,8 @@ public class UniteLobClient : MonoBehaviour
             clientSocket.BeginReceive(buff, 0, buff.Length, 0, CallBackReceive, buff);
             //접속했으면 접속한 넘버링 요구
             ReqClientNumber();
+            //
+            ReqRoomList();
         }
         catch
         {
@@ -100,11 +107,35 @@ public class UniteLobClient : MonoBehaviour
             ReqLobbyType reqType = (ReqLobbyType)recvData[0];
             if (reqType == ReqLobbyType.RoomMake)
             {
-                ResRoomJoin(recvData);
+                //
+                //0번 제외하고 만들어서 넘기기
+                byte[] roomDataByte = new byte[recvData[1 + 0]]; //0번빼고 룸데이터 패킷에서 방 정보 총길이 index 0
+                Buffer.BlockCopy(recvData, 1, roomDataByte, 0, roomDataByte.Length);
+                /*
+                 * roomData 패킷 바이트
+                    * [0] 이 방 정보의 총 길이
+                    * [1] 룸의 현재 인원 - 0이면 자신이 방장
+                    * [2] 룸포트 길이 2
+                    * [3] 방 이름 길이 
+                    * [4] 3번부터 2번 만큼 길이를 ushort로 변환한거 - 포트번호
+                    * [4+[2]] 부터 [3] 길이 만큼이 방이름
+                */
+
+                ResRoomJoin(roomDataByte);
             }
             else if (reqType == ReqLobbyType.ClientNumber)
             {
                 ResClientNumber(recvData);
+            }
+            else if (reqType == ReqLobbyType.RoomMakeFail)
+            {
+                //방 실패
+                ColorConsole.Default("방 접속 실패");
+                ResRoomJoinFail();
+            }
+            else if (reqType == ReqLobbyType.RoomList)
+            {
+                ResRoomList(recvData);
             }
 
             if (clientSocket.Connected)
@@ -119,8 +150,13 @@ public class UniteLobClient : MonoBehaviour
     #region 방 생성 진입
     private void ReqRoomJoin(string _roomName = "테스트 방 이름")
     {
+        
         ColorConsole.Default("로비에서 방 참가 신청");
-        string roomName = _roomName;
+        string roomName = inputText.text;
+        if(roomName == "")
+        {
+            roomName = _roomName;
+        }
         byte[] roomByte = Encoding.Unicode.GetBytes(roomName);
         byte[] reqRoom = new byte[roomByte.Length + 1];
         Array.Copy(roomByte, 0, reqRoom, 1, roomByte.Length); //룸 네임 전체를 요청 바이트 1번째부터 복사시작
@@ -148,33 +184,15 @@ public class UniteLobClient : MonoBehaviour
               * [3] 방 이름 길이 
               * [4] 4번부터 2번 만큼
               * [4+[2]] 부터 [3] 만큼
-              */
+        */
 
-        if (_receiveData[1] == LobbyServer.failCode)
-        {
-            //현재 인원수 쪽에 패일 코드를 넣어서 불가 체크
-            ColorConsole.Default("방에 참가 못했음");
-          //  Update(); //참가신청하는 순간 입력 while문이 꺼져서 다시 켜주기
-            return;
-        }
+        RoomData roomData = new RoomData(_receiveData);
 
-        //아이피 주소 파싱
-        int portLengthIdx = 2;
-        int portLength = _receiveData[portLengthIdx];
-        byte[] portByte = new byte[portLength];
-        int portIdx = 4;
-        for (int i = 0; i < portLength; i++)
-        {
-            portByte[i] = _receiveData[i + portIdx];
-        }
         //포트 번호 생성
-        ushort portNum = (ushort)BitConverter.ToUInt16(portByte);
+        ushort portNum = (ushort)roomData.portNum;
 
-        //방이름 파싱
-        int roomNameLength = _receiveData[3];
-        string roomName = Encoding.Unicode.GetString(_receiveData, portIdx + portLength, _receiveData[3]);
-
-        ColorConsole.Default(roomName + "방에서 내 순서 :" + _receiveData[1].ToString());
+        string roomName = roomData.roomName;
+        ColorConsole.Default(roomName + "방에서 내 순서 :" + roomData.curCount);
 
         isLobby = false; //게임으로 이동
 
@@ -184,17 +202,60 @@ public class UniteLobClient : MonoBehaviour
         clientSocket.Dispose();
 
         ColorConsole.Default("로비에서 플레이어 참가 클라이언트 생성 포트 번호 : " + portNum);
-        Action makeCallBack = () =>
+        Action makePlayClientCallback = () =>
         {
             PlayClient.ip = UniteServer.ServerIp.GetAddressBytes();
             PlayClient.id = id;
             PlayClient.port = portNum;
             SceneManager.LoadScene("PlayScene");
         };
-
-        callBackQue.Enqueue(makeCallBack);
-
+        callBackQue.Enqueue(makePlayClientCallback);
         meetState = MeetState.Room;
+    }
+
+    private void ResRoomJoinFail()
+    {
+        //현재 인원수 쪽에 패일 코드를 넣어서 불가 체크
+        ColorConsole.Default("방에 참가 못했음");
+        return;
+    }
+
+    private void ReqRoomList()
+    {
+        byte[] reqRoomList = new byte[] { (byte)ReqLobbyType.RoomList };
+        SendMessege(reqRoomList);
+    }
+
+    private void ResRoomList(byte[] _recvData)
+    {
+        /*
+         * [0]응답코드 roomList
+         * [1]방 리스트 수
+         * [2]부터 방정보
+         * 방정보  //방 데이터 만들어놓고, 방 참가할 수 있도록 요청한 애 한테 정보 전달
+               * [0] 이 방 정보의 총 길이
+               * [1] 룸의 현재 인원 - 0이면 자신이 방장
+               * [2] 룸포트 길이 2
+               * [3] 방 이름 길이 
+               * [4] 3번부터 2번 만큼 길이를 ushort로 변환한거 - 포트번호
+               * [4+[2]] 부터 [3] 길이 만큼이 방이름
+        */
+        int roomCount = _recvData[1];
+
+        if (roomCount == 0)
+        {
+            return;
+        }
+
+        int offSet = 2;
+        for (int i = 0; i < _recvData[1]; i++)
+        {
+            byte[] roomDataByte = new byte[_recvData[offSet]];
+            Buffer.BlockCopy(_recvData, offSet, roomDataByte, 0, _recvData[offSet]);
+            RoomData room = new RoomData(roomDataByte);
+            offSet += _recvData[offSet];
+            ColorConsole.Default(room.roomName + "방 존재");
+        }
     }
     #endregion
 
@@ -242,6 +303,5 @@ public class UniteLobClient : MonoBehaviour
     }
 
     bool isLobby = true;
-
 }
 
