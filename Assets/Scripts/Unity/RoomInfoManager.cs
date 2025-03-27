71463c58197cfa2ad75b3bb0d06b21b4f08b4ecd
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-27 11:09:03 +0900

    [추가] 룸 정보 표기 ui

---
+++ Assets/Scripts/Unity/RoomInfoManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomInfoManager : MonoBehaviour
+{
+    public TMP_Text m_roomNameText; //방 제목
+    public TMP_Text m_partyText; //참가원
+    public TMP_Text m_turnText; //지금 누구차레
+    public TMP_Text m_preCard; //지금 놓여있는 카드
+  
+
+}
