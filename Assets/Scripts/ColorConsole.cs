4415fbb25c71fc9d637ca699653ee450372f6d88
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-27 16:16:33 +0900

    [수정] 안쓰는 usign

--- Assets/Scripts/ColorConsole.cs
+++ Assets/Scripts/ColorConsole.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Unity.VisualScripting;
-using UnityEditor.VersionControl;
+
 
 public class ColorConsole
 {
