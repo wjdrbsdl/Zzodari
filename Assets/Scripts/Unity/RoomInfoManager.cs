753f2f7e6e7ce03aede936ee84238fe96b11624f
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-04-09 16:08:35 +0900

    [추가] 게임종료시 게임기록 출력

--- Assets/Scripts/Unity/RoomInfoManager.cs
+++ Assets/Scripts/Unity/RoomInfoManager.cs
@@ -86,6 +86,10 @@ public class RoomInfoManager : MonoBehaviour
                 case ReqRoomType.StageOver:
                     m_badPoint.text = InputColor("¹úÁ¡ : ") + m_client.inGameData.badPoint.ToString();
                     break;
+                case ReqRoomType.GameOver:
+                    m_badPoint.text = InputColor("¹úÁ¡ : ") + m_client.inGameData.badPoint.ToString();
+                    m_badPoint.text += InputColor(" ¼øÀ§ : ") + m_client.inGameData.myRank.ToString();
+                    break;
                 case ReqRoomType.Draw:
                     isShow = true;
                     restTime = showTime;
