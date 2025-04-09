970d4077b387aac0cf9d8880cc25473398a8ea85
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-04-09 14:40:55 +0900

    [추가] 안쓰는 using 제거

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.Apple.ReplayKit;
-
 
 public enum BGMType
 {
