c99b1dd96077935f1bfb8cf136ea3976c75bd1bb
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-06-11 15:46:15 +0900

    [추가] DI 싼타와 아이들 구조

---
+++ Assets/Scripts/Unity/04.Utils/Container.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container
+{
+    public Container()
+    {
+        _registration = new Dictionary<Type, object>();
+    }
+
+    Dictionary<Type, object> _registration;
+
+    /// <summary>
+    /// 생성자가 있는 일반 C# 클래스 등록 (생성해서 추가함)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public void Register<T>() where T : class, new()
+    {
+        //등록할 인스턴스가 없으면 만들어서 넣기(생성자가 필요 -> new() 제한자)
+        T obj = new T(); // new() 제한자로인해서 생성자 있는 녀석만 들어와서 
+        _registration[typeof(T)] = obj;
+    }
+
+    /// <summary>
+    /// Monobehaviour 객체 생성하여 추가
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public void RegisterMonobehaviour<T>() where T : MonoBehaviour
+    {
+        T obj = new GameObject(typeof(T).Name).AddComponent<T>();
+        _registration[typeof(T)] = obj;
+    }
+
+
+    /// <summary>
+    /// 하이어키에 존재하는 모노비해비어 객체 추가 
+    /// </summary>
+    /// <param name="monobehaviour"></param>
+    public void RegisterMonobehaviour(MonoBehaviour monobehaviour)
+    {
+        _registration[monobehaviour.GetType()] = monobehaviour;
+    }
+
+    public T Resolve<T>()
+    {
+        return (T)_registration[typeof(T)];
+    }
+}
