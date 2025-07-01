d7eae08202859231ac3887a97a5add6be625bb93
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-07-01 13:38:32 +0900

    [수정] 빌드 세팅

--- .utmp/RelWithDebInfo/156j3l2b/armeabi-v7a/FramePacing/cmake_install.cmake
+++ .utmp/RelWithDebInfo/156j3l2b/armeabi-v7a/FramePacing/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Work/L20250210/Zzodari/Library/Bee/Android/Prj/Mono2x/Gradle/unityLibrary/src/main/cpp/FramePacing
+# Install script for directory: C:/Work/L20250210/Zzodari/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp/FramePacing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
--- .utmp/RelWithDebInfo/156j3l2b/armeabi-v7a/cmake_install.cmake
+++ .utmp/RelWithDebInfo/156j3l2b/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Work/L20250210/Zzodari/Library/Bee/Android/Prj/Mono2x/Gradle/unityLibrary/src/main/cpp
+# Install script for directory: C:/Work/L20250210/Zzodari/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
