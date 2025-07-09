8aaf5bb84a03215ddf117a1da5c60a9c9816efb0
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-07-09 09:45:06 +0900

    [수정] 패킷 패킹부분 따로 분리

--- Assets/Scripts/TcpClient/NetworkManager.cs
+++ Assets/Scripts/TcpClient/NetworkManager.cs
@@ -91,21 +91,29 @@ public class NetworkManager
         // DebugManager.instance.EnqueMessege("매니저에서 샌드");
         ushort length = (ushort)data.Length;
         byte[] header = EndianChanger.HostToNet(length);
-        byte[] packet = new byte[header.Length + data.Length];
-        Buffer.BlockCopy(header, 0, packet, 0, header.Length);
-        Buffer.BlockCopy(data, 0, packet, header.Length, data.Length);
-        //clientSocket.Send(packet);
-
+        byte[] packet = Packing(data);
 
         int rest = (length + header.Length);
         int send = 0;
         do
         {
+            OnDetectDisConnect?.Invoke("보내기" + rest + "양");
             byte[] sendPacket = new byte[rest];
             Buffer.BlockCopy(packet, packet.Length - rest, sendPacket, 0, rest);
             send = clientSocket.Send(sendPacket);
             rest -= send;
         } while (rest >= 1);
+    }
+
+    private byte[] Packing(byte[] data)
+    {
+        ushort length = (ushort)data.Length;
+        byte[] header = EndianChanger.HostToNet(length);
+        byte[] packet = new byte[header.Length + data.Length];
+        Buffer.BlockCopy(header, 0, packet, 0, header.Length);
+        Buffer.BlockCopy(data, 0, packet, header.Length, data.Length);
+
+        return packet;
     }
 
     private bool clearDis = false;
@@ -123,8 +131,11 @@ public class NetworkManager
     public void InvalidDisConnect()
     {
         //테스트를 위해 ip 전환처럼 클라 요청없이 연결이 끊긴경우
-        clientSocket?.Close();
-        clientSocket?.Dispose();
+        if(clientSocket != null)
+        {
+            clientSocket?.Close();
+            clientSocket?.Dispose();
+        }
         clientSocket = null;
     }
 
@@ -140,19 +151,7 @@ public class NetworkManager
                     
             await Task.Delay(3000); //3초마다 확인
             OnDetectDisConnect?.Invoke("핑퐁 보내기 확인");
-            //try
-            //{
-            //    if (UnityEngine.Application.internetReachability == UnityEngine.NetworkReachability.NotReachable)
-            //    {
-            //        UnityEngine.Debug.Log("인터넷 되는지 확인");
-            //        //OnDetectDisConnect?.Invoke();
-            //    }
-            //}
-            //catch
-            //{
-            //    UnityEngine.Debug.Log("이벤트 인터넷 되는지 캐치");
-            //}
-
+     
 
             if (clearDis == true)
             {
@@ -162,18 +161,20 @@ public class NetworkManager
 
             try
             {
-                int sendVlaue = clientSocket.Send(new byte[] { 213 }); //핑퐁용
-                if (sendVlaue == 0)
+                byte[] pong = new byte[] { 213 };
+                byte[] packet = Packing(pong);
+                int sendValue = clientSocket.Send(packet);
+                if (sendValue == 0)
                 {
                     //UnityEngine.Debug.Log("소켓 끊긴거 확인 재연결");
                     OnDetectDisConnect?.Invoke("보낼게 없어");
                     Connect(); //연결했던 소켓에 재연결 시도
                     break;
                 }
-                OnDetectDisConnect?.Invoke("핑퐁 보내기 보낸 수 " + sendVlaue);
-            }
+           }
             catch
             {
+                InvalidDisConnect();
                 OnDetectDisConnect?.Invoke("소켓이 망했음");
                 Connect(); //연결했던 소켓에 재연결 시도
                 break;
